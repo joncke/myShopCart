47ba6ad45f3efaea82c9a3c72820e5cc2fa8462d
jonkse <jonkse@163.com>
2017-12-14 15:50:53 +0800

    v-on money filters

--- js/cart.js
+++ js/cart.js
@@ -1,14 +1,21 @@
 /**
  * Created by Administrator on 2017/12/14 0014.
  */
-var vm = new Vue({
+new Vue({
     el:'#app',
     data:{
         totalMoney:0,
         productList:[]
     },
+    filters:{
+        formatMoney:function (value) {
+            return "￥ " + value.toFixed(2);
+        }
+    },
     mounted: function () {
-        this.carView()
+        this.$nextTick(function () {//vm=this
+            this.carView()
+        })
     },
     methods:{
         carView:function () {
@@ -17,7 +24,16 @@ var vm = new Vue({
                 _this.productList = res.body.result.list;
                 _this.totalMoney = res.body.result.totalMoney;
             });
+            //也可以使用箭头函数，直接指向外部的this
+            // this.$http.get("data/cartdata.json",{"id":123}).then(res=> {
+            //     this.productList = res.body.result.list;
+            //     this.totalMoney = res.body.result.totalMoney;
+            // });
         }
     }
 
 });
+//全局过滤器
+Vue.filter('money',function (value, type) {
+    return  "￥ " + value.toFixed(2) + type;
+})
